fb8fd7961a951e6ab8a7f93b40efd710254e5442
Janel <janel.me@yahoo.com>
2021-04-18 08:43:33 +0430

    cleaned and other documentions added

--- parameters.py
+++ parameters.py
@@ -19,6 +19,8 @@ def distribution_of_daily_usage():
     This function generates a random number as product usage
     in a single day, i.e. it may produce number 5 with probability
     of 10% also it shows 5 products used at inventory!
+
+    HINT: YOU CAN PERSONALIZE THIS PARAMETER TO GET YOUR RESULT
     :return:
     Integer number in range 0 - 5 that shows usage of
     product at inventory in that day.
@@ -45,6 +47,15 @@ def distribution_of_daily_usage():
 
 
 def distribution_of_lt():
+    """
+    LT is load time
+    The time that you have to wait to receive your orders
+
+    definitely you can personalize this parameters too
+    :return:
+    days to wait for orders to receive as integer number
+    """
+
     random_number = random.randint(1, 10)
     if 1 <= random_number <= 2:
         return 1
